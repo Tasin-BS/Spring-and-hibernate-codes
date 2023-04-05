e47ae47dd26fa7a041dd6d8be730848d5947942f
Tasin <tasin.99bd@gmail.com>
2023-04-05 18:50:25 +0600

    MVC and Setup Environment

---
+++ spring-mvc-demo/src/main/java/springDemo/MVC/HomeController.java
@@ -0,0 +1,14 @@
+package springDemo.MVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showpage()
+	{
+		return "main-menu";
+	}
+}
